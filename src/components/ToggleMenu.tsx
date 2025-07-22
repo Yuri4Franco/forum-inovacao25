3cd9fe8b506583106dccdb53d7d40af5a517411e
Yuri4Franco <interyurinho@gmail.com>
2025-07-22 20:52:26 -0300

    adoraria arrumar esse menu

--- src/components/ToggleMenu.tsx
+++ src/components/ToggleMenu.tsx
@@ -1,24 +1,36 @@
 import "./ToggleMenu.css";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import MenuLateral from "./MenuLateral";
 
 function ToggleMenu() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const checkboxRef = useRef<HTMLInputElement>(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
 
+  const closeMenu = () => {
+    setMenuOpen(false);
+    if (checkboxRef.current) {
+      checkboxRef.current.checked = false; // força o botão a voltar pro "hambúrguer"
+    }
+  };
   return (
     <div className="toggle-menu">
-      <input type="checkbox" id="checkbox" onChange={toggleMenu} />
+      <input
+        type="checkbox"
+        id="checkbox"
+        onChange={toggleMenu}
+        ref={checkboxRef}
+      />
       <label htmlFor="checkbox" className="toggle">
         <div className="bars" id="first"></div>
         <div className="bars" id="second"></div>
         <div className="bars" id="third"></div>
       </label>
 
-      <MenuLateral isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
+      <MenuLateral isOpen={menuOpen} onClose={closeMenu} />
     </div>
   );
 }
