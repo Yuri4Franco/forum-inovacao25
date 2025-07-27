86ee0a45e73ad8a5663694f6e332cb18160c7a87
Yuri4Franco <interyurinho@gmail.com>
2025-07-27 19:10:21 -0300

    lol

--- src/components/SliderReact.tsx
+++ src/components/SliderReact.tsx
@@ -1,7 +1,6 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./SliderReact.css";
-
 import Slider from "react-slick";
 import { trajetoria } from "../consts/Trajetoria";
 import TimeLineForum from "./TimeLineForum";
@@ -20,11 +19,27 @@ function SliderReact() {
   return (
     <div className="slider-react-container">
       <Slider {...settings}>
+        <div className="timeline-capa">
+          <h1>nossa trajetória</h1>
+          <h2>
+            Fórum de <span className="break">Inovação</span>
+          </h2>
+          <p>arrasta para o lado</p>
+          <div className="setas-animadas">
+            <span>&gt;</span>
+            <span>&gt;</span>
+            <span>&gt;</span>
+            <span>&gt;</span>
+            <span>&gt;</span>
+          </div>
+        </div>
         {trajetoria.map((item, index) => (
           <div key={index}>
             <TimeLineForum item={item} />
           </div>
         ))}
+
+        {/* <div className="slider-fim"></div> */}
       </Slider>
     </div>
   );
